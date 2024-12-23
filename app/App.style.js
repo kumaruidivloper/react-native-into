32fe72ffe1efbcab83637755519dda631282711c
kumaruidivloper <kumaruidivloper@gmail.com>
2024-12-23 15:46:43 +1100

    Initial commit

---
+++ app/App.style.js
@@ -0,0 +1,19 @@
+import { StyleSheet } from "react-native";
+export const styleObj = StyleSheet.create({
+  title: {
+    color: "#ffffff",
+    fontSize: 20,
+    marginTop: 50,
+    marginLeft: "50%",
+  },
+  square: {
+    height: 100,
+    backgroundColor: "green",
+    width: 100,
+  },
+  innerTxt: {
+    color: "white",
+    marginTop: 30,
+    marginLeft: 40,
+  },
+});
