a5c7ee3dfe8ef9dcdeebdd0e8e04425534c5331b
kumaruidivloper <kumaruidivloper@gmail.com>
2024-12-24 06:34:46 +1100

    flex-box system

--- app/App.style.js
+++ app/App.style.js
@@ -1,19 +1,7 @@
 import { StyleSheet } from "react-native";
+
 export const styleObj = StyleSheet.create({
-  title: {
-    color: "#ffffff",
-    fontSize: 20,
-    marginTop: 50,
-    marginLeft: "50%",
-  },
-  square: {
-    height: 100,
-    backgroundColor: "green",
-    width: 100,
-  },
-  innerTxt: {
-    color: "white",
-    marginTop: 30,
-    marginLeft: 40,
-  },
+  box1: { height: 100, width: 100, backgroundColor: "red" },
+  box2: { height: 100, width: 100, backgroundColor: "orange" },
+  box3: { height: 100, width: 100, backgroundColor: "yellow" },
 });
